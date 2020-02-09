4472d0042ce425cb3849080c93159565f60084a2
Krzysztof Tomczyk <ktomczyk1122@gmail.com>
2020-02-09 20:55:20 +0100

    Almost succes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using System.Linq;
+using System;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
 
 namespace YouTubeInfo
 {
     class Program //TODO how can i track variables that I need to recive from user?
     {
         static string videoName;
-        static string queryLink;
-        static void Main(string[] args)
+        static string url;
+        static string html;
+        static dynamic video;
+        static HttpClient httpClient;
+        static HtmlDocument htmlDocument;
+        static async Task Main(string[] args)
         {
             //1. Show logo
             Console.WriteLine("=== YouTubeInfo! ==="); //TODO make a unicode logo
@@ -16,20 +24,31 @@ namespace YouTubeInfo
             videoName = Console.ReadLine();
 
             //3. Prepare search link
-            //3.1 Replace spaces with pluses //TODO do i need to change special chars?
-            videoName = videoName.Replace(" ","+");
-            //3.2 Put it do link
-            queryLink = "https://www.youtube.com/results?search_query=" + videoName;
+            videoName = videoName.Replace(" ", "+");//TODO do i need to change special chars?
+            url = "https://www.youtube.com/results?search_query=" + videoName;
 
             //4. Parse HTML from the link
-            // https://stackoverflow.com/questions/16642196/get-html-code-from-website-in-c-sharp
+            await GetHtmlAsync();
 
             //5. Get link of the 1st search result
-            // https://stackoverflow.com/questions/8548579/library-to-extract-data-from-html-string
+            //TODO learn about async and wait
 
             //6. Do 4th step but with different link
 
             //7. Get specific data from video
+            
+        }
+
+        private static async Task GetHtmlAsync()
+        {
+            httpClient = new HttpClient();
+            html = await httpClient.GetStringAsync(url);
+            htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            video = htmlDocument.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("id","")
+                .Equals("dismissable"));
         }
     }
 }
